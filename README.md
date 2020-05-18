b81cf86cffa48c9e06a2fa988d5b681e740d7ee6
root <real4suraj2@gmail.com>
2020-05-19 01:38:34 +0530

    Update README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scheduler
+
+## Note : Configures `constants/` directory for `social-config.js`
---
+++ constants/social-config.js
@@ -0,0 +1,5 @@
+const FACEBOOK_APP_ID = "860244124496095";
+const GOOGLE_ANDROID_ID =
+	"746591055474-uva2rfpk4cje6pe57ottjpcnirfaifab.apps.googleusercontent.com";
+
+export { FACEBOOK_APP_ID, GOOGLE_ANDROID_ID };
