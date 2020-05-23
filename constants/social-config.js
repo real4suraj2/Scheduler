18f763e200fc6daf6ae2805517e867e667d72828
Suraj <48567316+real4suraj2@users.noreply.github.com>
2020-05-23 13:59:51 +0530

    Delete social-config.js

--- constants/social-config.js
+++
@@ -1,8 +0,0 @@
-const FACEBOOK_APP_ID = "860244124496095";
-const GOOGLE_ANDROID_ID =
-	"746591055474-uva2rfpk4cje6pe57ottjpcnirfaifab.apps.googleusercontent.com";
-
-const GOOGLE_STANDALONE_APP_ID =
-	"746591055474-naabq16tlgbu4tv5u2f6tktsijvf7a7v.apps.googleusercontent.com";
-
-export { FACEBOOK_APP_ID, GOOGLE_ANDROID_ID, GOOGLE_STANDALONE_APP_ID };
